9ea9959b6e5d7fb943302e25b956315b961863d2
ps26pate <ps26pate@uwaterloo.ca>
2021-08-27 15:09:54 -0400

    added normative data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import data from "./normative-data.json";
 
 function App() {
+  console.log(data["2MWT"].Men);
   return (
     <div className='App'>
       <h1> Parmin is cool</h1>
